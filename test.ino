f1070c8aa77f87400d60d91e83fabf516edb91e2
Dexioz <128942578+Dexioz@users.noreply.github.com>
2023-03-26 04:18:43 +0200

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,61 +1,89 @@
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_RA8875.h>
-#include <Wire.h>
-#include <GSL1680.h>
+#include <AccelStepper.h>
 
-// RA8875 connections
-#define RA8875_INT 4
-#define RA8875_CS 10
-#define RA8875_RESET 9
+// Define the pins for the stepper motor driver
+#define STEP_PIN 9
+#define DIR_PIN 8
+#define ENABLE_PIN 10
 
-Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
-GSL1680 touchscreen = GSL1680();
+// Define the pins for the manual encoder
+#define ENCODER_A_PIN 2
+#define ENCODER_B_PIN 3
 
-// Joystick connections
-#define JOYSTICK_X A0
-#define JOYSTICK_Y A1
-#define JOYSTICK_Z 2
-#define JOYSTICK_SW 3
-#define JOYSTICK_DEADZONE 20
+// Define the number of steps per revolution for the stepper motor
+#define STEPS_PER_REV 200
 
-// Manual encoders connections
-#define ENCODER1_CLK 4
-#define ENCODER1_DT 5
-#define ENCODER1_SW 6
-#define ENCODER2_CLK 7
-#define ENCODER2_DT 8
-#define ENCODER2_SW 12
-#define ENCODER3_CLK 13
-#define ENCODER3_DT 22
-#define ENCODER3_SW 23
+// Define the number of microsteps per step for the stepper driver
+#define MICROSTEPS 1000
 
-// Emergency stop button connection
-#define EMERGENCY_STOP 11
+// Define the maximum speed and acceleration for the stepper motor
+#define MAX_SPEED 5000
+#define ACCELERATION 500
 
-// Stepper motors connections
-#define MOTOR1_STEP 24
-#define MOTOR1_DIR 25
-#define MOTOR2_STEP 26
-#define MOTOR2_DIR 27
-#define MOTOR3_STEP 28
-#define MOTOR3_DIR 29
+// Define the movement scale values for the encoder
+#define SCALE_1 1
+#define SCALE_2 5
+#define SCALE_3 25
 
-// Initialize joystick state variables
-int joystickX = 0;
-int joystickY = 0;
-int joystickZ = 0;
-int joystickSW = 0;
+// Initialize the stepper motor object
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
-// Initialize manual encoder state variables
-int encoder1Pos = 0;
-int encoder2Pos = 0;
-int encoder3Pos = 0;
-int encoder1Last = 0;
-int encoder2Last = 0;
-int encoder3Last = 0;
-int encoder1Scale = 1;
-int encoder2Scale = 1;
-int encoder3Scale = 1;
+// Initialize variables for the manual encoder
+volatile int encoderPosition = 0;
+volatile int lastEncoderPosition = 0;
+volatile int encoderDirection = 0;
+volatile int encoderScale = SCALE_1;
 
+// Interrupt service routine for the manual encoder
+void encoderISR() {
+  int a = digitalRead(ENCODER_A_PIN);
+  int b = digitalRead(ENCODER_B_PIN);
+  if (a == b) {
+    encoderDirection = 1;
+  } else {
+    encoderDirection = -1;
+  }
+}
 
+// Setup function
+void setup() {
+  // Set up the stepper motor driver pins
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(ENABLE_PIN, OUTPUT);
+  digitalWrite(ENABLE_PIN, LOW);
+
+  // Set up the manual encoder pins
+  pinMode(ENCODER_A_PIN, INPUT_PULLUP);
+  pinMode(ENCODER_B_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A_PIN), encoderISR, CHANGE);
+
+  // Set up the serial port
+  Serial.begin(9600);
+
+  // Set up the stepper motor object
+  stepper.setMaxSpeed(MAX_SPEED);
+  stepper.setAcceleration(ACCELERATION);
+
+  // Move the stepper motor to the home position
+  stepper.moveTo(0);
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
+}
+
+// Main loop
+void loop() {
+  // Check if the encoder direction has changed
+  if (encoderDirection != 0) {
+    // Calculate the number of steps to move the stepper motor
+    int steps = encoderDirection * encoderScale * MICROSTEPS;
+    // Move the stepper motor by the calculated number of steps
+    stepper.move(steps);
+    // Update the last encoder position and direction
+    lastEncoderPosition = encoderPosition;
+    encoderDirection = 0;
+  }
+
+  // Run the stepper motor
+  stepper.run();
+}
